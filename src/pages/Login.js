c879aa0e9ce673ce6261995fcb92afd820901221
sraghav42 <sraghav42@gmail.com>
2024-04-23 23:03:51 -0400

    Updates to the login.js page to use JSX

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,28 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+    const [username,setUsername]=useState('');
+    const [password,setPassword]=useState('');
+    const [error,setError]=useState('');
 
+    const handleSubmit= async (e) => {
+        e.preventDefault();
+        setError('');
+        
+        try{
+            const res=await fetch('http://localhost:5000/api/auth/login',{
+                method:'POST',
+                body:JSON.stringify({username,password}),
+                headers:{'Content-Type':'application/json'}
+            });
+
+            const data=await res.json();
+            if(res.status===400 || res.status===401 || res.status===404){
+                setError(`${data.message}. ${data.error ? data.error : ''}`);
+            } else{
+                window.location.assign('/');
+            }
+        } catch(err){
+            console.log("Error : ",err);
+            setError('An unexpected error occurred. Please try again.');
+        }
+    };
     return (
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Login Page</title>
-        </head>
-        <body>
-            <h1>Login Page</h1>
-            <form>
-            <div class="error"></div>
+      <div>
+        <h1>Login Page</h1>
+        <form onSubmit={handleSubmit}>
+            {error && <div className='error' style={{backgroundColor:'red'}}>{error}</div>}
             <br />
-            <label for="username">Username</label><br />
-            <input type="text" id="username" required /><br />
-            <label for="password">Password</label><br />
-            <input type="password" id="password" required /><br />
-            <input type="submit" value="login" /><br />
-            </form>
-            <a href="/register">Don't have an accout? Register</a>
-        </body>
-        </html>
+            <label htmlFor='username'>Username</label>
+            <br />
+            <input type='text' id='username' value={username} onChange={(e) => setUsername(e.target.value)} required />
+            <br />
+            <label htmlFor='password'>Password</label>
+            <br />
+            <input type='password' id='password' value={password} onChange={(e) => setPassword(e.target.value)} required />
+            <br />
+            <input type='submit' value='login' />
+            <br />
+        </form>
+        <a href='/register'>Don't have an account? Register</a>
+      </div>  
     );
 };
 
